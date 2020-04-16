9722d68ae3b75ba3a9e2bd59b1e908c476b3f5dd
wasuken <wevorence@gmail.com>
2020-04-16 23:20:28 +0900

    途中。

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::io::*;
-use std::str::FromStr;
 use std::cmp;
-
-fn read<T: FromStr>() -> T {
-    let stdin = stdin();
-    let stdin = stdin.lock();
-    let token: String = stdin
-        .bytes()
-        .map(|c| c.expect("failed to read char") as char)
-        .skip_while(|c| c.is_whitespace())
-        .take_while(|c| !c.is_whitespace())
-        .collect();
-    token.parse().ok().expect("failed to parse token")
-}
+use queue::Queue;
 
 fn act1_6_1(n: i32, a: Vec<u32>) -> u32 {
 	for x in &a {
@@ -39,7 +26,96 @@ fn act1_6_2(l: i32, n: i32, x: Vec<i32>) -> (i32, i32) {
 	return (min, max)
 }
 
+fn dfs(i: i32, sum: i32, n:i32, a: &Vec<i32>, k: i32) -> bool {
+	if i == n {
+		return sum == k
+	}
+	if dfs(i + 1, sum, n,  a, k) {
+		return true
+	}
+	if dfs(i + 1, sum + a[i as usize], n, a, k) {
+		return true
+	}
+	return false;
+}
 
+fn act2_1_1(n: i32, a: &Vec<i32>, k: i32) -> String {
+	if dfs(0, 0, n, a, k) {
+		return "Yes".to_owned()
+	}else{
+		return "No".to_owned()
+	}
+}
+
+fn act2_1_2_dfs(x: i32, y: i32, n: i32, m: i32, field: &mut Vec<Vec<String>>) {
+	field[x as usize][y as usize] = ".".to_owned();
+	for dx in -1..2 {
+		for dy in -1..2 {
+			let nx = x + dx;
+			let ny = y + dy;
+
+			if 0 <= nx && nx < n && 0 <= ny && ny < m && field[nx as usize][ny as usize] == "W" {
+				act2_1_2_dfs(nx, ny, n, m, field);
+			}
+		}
+	}
+}
+
+fn act2_1_2(n: i32, m: i32, field: &mut Vec<Vec<String>>) -> i32 {
+	let mut res = 0;
+	for i in 0..n {
+		for j in 0..m {
+			if field[i as usize][j as usize] == "W" {
+				act2_1_2_dfs(i, j, n, m, field);
+				res+=1;
+			}
+		}
+	}
+	return res
+}
+
+fn act2_1_3_bfs(sx: i32, sy: i32, gx: i32, gy: i32, maze: &mut Vec<Vec<String>>, d: Vec<Vec<i32>>) -> i32 {
+	let mut q = Queue::with_capacity(1000000);
+	for i in 0..maze.len() {
+		for j in 0..maze[0].len() {
+
+		}
+	}
+	q.queue((sx, sy)).unwrap();
+	d[sx as usize][sy as usize] = 0;
+	while !q.is_empty() {
+		let ptr = match q.dequeue() {
+			Some(pt) => pt,
+			_ => (0, 0)
+		};
+		if ptr.0 == gx && ptr.1 == gy {
+			break;
+		}
+	}
+}
+
+fn act2_1_3(n: i32, m: i32, maze: &mut Vec<Vec<String>>) -> i32 {
+	let mut sx: i32 = 0;
+	let mut sy: i32 = 0;
+	let mut gx: i32 = 0;
+	let mut gy: i32 = 0;
+	let mut d = Vec::new();
+
+	for x in 0..maze.len() {
+		for y in 0..maze[x].len() {
+			if maze[x as usize][y as usize] == "S" {
+				sx = x as i32;
+				sy = y as i32;
+			}
+			if maze[x as usize][y as usize] == "G" {
+				gx = x as i32;
+				gy = y as i32;
+			}
+		}
+	}
+
+	return act2_1_3_bfs(sx, sy, gx, gy, maze, d)
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +128,56 @@ mod tests {
 	#[test]
 	fn act1_6_2_test(){
 		assert_eq!(act1_6_2(10, 3, vec![2, 6, 7]), (4, 8));
+	}
+	#[test]
+	fn act2_1_1_test(){
+		assert_eq!(act2_1_1(4, &vec![1, 2, 4, 7], 13), "Yes");
+		assert_eq!(act2_1_1(4, &vec![1, 2, 4, 7], 15), "No");
+	}
+	#[test]
+	fn act2_1_2_test(){
+		let mut input = vec![vec!["W",".",".",".",".",".",".",".",".","W","W","."],
+							 vec![".","W","W","W",".",".",".",".",".","W","W","W"],
+							 vec![".",".",".",".","W","W",".",".",".","W","W","."],
+							 vec![".",".",".",".",".",".",".",".",".","W","W","."],
+							 vec![".",".",".",".",".",".",".",".",".","W",".","."],
+							 vec![".",".","W",".",".",".",".",".",".","W",".","."],
+							 vec![".","W",".","W",".",".",".",".",".","W","W","."],
+							 vec!["W",".","W",".","W",".",".",".",".",".","W","."],
+							 vec![".","W",".","W",".",".",".",".",".",".","W","."],
+							 vec![".",".","W",".",".",".",".",".",".",".","W","."]
+		];
+		let mut sInput = Vec::new();
+		for i in input {
+			let mut temp = Vec::new();
+			for j in i {
+				temp.push(j.to_owned())
+			}
+			sInput.push(temp)
+		}
+		assert_eq!(act2_1_2(10, 12, &mut sInput), 3);
+	}
+	#[test]
+	fn act2_1_3_test(){
+		let mut input = vec![vec!["#","S","#","#","#","#","#","#",".","#"],
+							 vec![".",".",".",".",".",".","#",".",".","#"],
+							 vec![".","#",".","#","#",".","#","#",".","#"],
+							 vec![".","#",".",".",".",".",".",".",".","."],
+							 vec!["#","#",".","#","#",".",".",".",".","."],
+							 vec![".",".",".",".","#",".",".",".",".","."],
+							 vec![".",".",".",".","#",".",".",".",".","."],
+							 vec![".",".",".",".","#",".",".",".",".","."],
+							 vec![".","#","#","#","#",".","#","#","#","."],
+							 vec![".",".",".",".","#",".",".",".","G","#"]
+		];
+		let mut sInput = Vec::new();
+		for i in input {
+			let mut temp = Vec::new();
+			for j in i {
+				temp.push(j.to_owned())
+			}
+			sInput.push(temp)
+		}
+		assert_eq!(act2_1_2(10, 10, &mut sInput), 22);
 	}
 }
