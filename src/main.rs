6f465f065f45f52a824af7ce7e5345686f6dce15
wasuken <wevorence@gmail.com>
2020-04-17 14:37:39 +0900

    finished 2-1.

--- src/main.rs
+++ src/main.rs
@@ -49,10 +49,10 @@ fn act2_1_1(n: i32, a: &Vec<i32>, k: i32) -> String {
 
 fn act2_1_2_dfs(x: i32, y: i32, n: i32, m: i32, field: &mut Vec<Vec<String>>) {
 	field[x as usize][y as usize] = ".".to_owned();
-	for dx in -1..2 {
-		for dy in -1..2 {
-			let nx = x + dx;
-			let ny = y + dy;
+	for x_ in -1..2 {
+		for y_ in -1..2 {
+			let nx = x + x_;
+			let ny = y + y_;
 
 			if 0 <= nx && nx < n && 0 <= ny && ny < m && field[nx as usize][ny as usize] == "W" {
 				act2_1_2_dfs(nx, ny, n, m, field);
@@ -74,24 +74,39 @@ fn act2_1_2(n: i32, m: i32, field: &mut Vec<Vec<String>>) -> i32 {
 	return res
 }
 
-fn act2_1_3_bfs(sx: i32, sy: i32, gx: i32, gy: i32, maze: &mut Vec<Vec<String>>, d: Vec<Vec<i32>>) -> i32 {
+const INF: i32 = 100000000;
+
+fn act2_1_3_bfs(sx: i32, sy: i32, gx: i32, gy: i32, maze: &mut Vec<Vec<String>>, d: &mut Vec<Vec<i32>>) -> i32 {
+	let dx: Vec<i32> = [1, 0, -1, 0].to_vec();
+	let dy: Vec<i32> = [0, 1, 0, -1].to_vec();
 	let mut q = Queue::with_capacity(1000000);
 	for i in 0..maze.len() {
 		for j in 0..maze[0].len() {
-
+			d[i as usize][j as usize] = INF;
 		}
 	}
 	q.queue((sx, sy)).unwrap();
 	d[sx as usize][sy as usize] = 0;
 	while !q.is_empty() {
-		let ptr = match q.dequeue() {
+		let p = match q.dequeue() {
 			Some(pt) => pt,
 			_ => (0, 0)
 		};
-		if ptr.0 == gx && ptr.1 == gy {
+		if p.0 == gx && p.1 == gy {
 			break;
 		}
+		for i in 0..4{
+			let nx = p.0 + dx[i];
+			let ny = p.1 + dy[i];
+
+			if 0 <= nx && (nx as usize) < maze.len() && 0 <= ny && (ny as usize) < maze[0].len()
+				&& maze[nx as usize][ny as usize] != "#" && d[nx as usize][ny as usize] == INF {
+					q.queue((nx, ny)).unwrap();
+					d[nx as usize][ny as usize] = d[p.0 as usize][p.1 as usize] + 1;
+				}
+		}
 	}
+	return d[gx as usize][gy as usize]
 }
 
 fn act2_1_3(n: i32, m: i32, maze: &mut Vec<Vec<String>>) -> i32 {
@@ -99,10 +114,12 @@ fn act2_1_3(n: i32, m: i32, maze: &mut Vec<Vec<String>>) -> i32 {
 	let mut sy: i32 = 0;
 	let mut gx: i32 = 0;
 	let mut gy: i32 = 0;
-	let mut d = Vec::new();
+	let d: &mut Vec<Vec<i32>> = &mut Vec::new();
 
 	for x in 0..maze.len() {
+		d.push(Vec::new());
 		for y in 0..maze[x].len() {
+			d[x].push(INF);
 			if maze[x as usize][y as usize] == "S" {
 				sx = x as i32;
 				sy = y as i32;
@@ -163,9 +180,9 @@ mod tests {
 							 vec![".",".",".",".",".",".","#",".",".","#"],
 							 vec![".","#",".","#","#",".","#","#",".","#"],
 							 vec![".","#",".",".",".",".",".",".",".","."],
-							 vec!["#","#",".","#","#",".",".",".",".","."],
-							 vec![".",".",".",".","#",".",".",".",".","."],
-							 vec![".",".",".",".","#",".",".",".",".","."],
+							 vec!["#","#",".","#","#",".","#","#","#","#"],
+							 vec![".",".",".",".","#",".",".",".",".","#"],
+							 vec![".","#","#","#","#","#","#","#",".","#"],
 							 vec![".",".",".",".","#",".",".",".",".","."],
 							 vec![".","#","#","#","#",".","#","#","#","."],
 							 vec![".",".",".",".","#",".",".",".","G","#"]
@@ -178,6 +195,6 @@ mod tests {
 			}
 			sInput.push(temp)
 		}
-		assert_eq!(act2_1_2(10, 10, &mut sInput), 22);
+		assert_eq!(act2_1_3(10, 10, &mut sInput), 22);
 	}
 }
