ab9d7c802539ba0d94c80e496f129a7039890fb9
wasuken <wasuken1@gmail.com>
2020-06-19 17:52:52 +0900

    until 2-5-3.

--- src/act2/chap5.rs
+++ src/act2/chap5.rs
@@ -1,5 +1,6 @@
 extern crate num;
 use std::collections::VecDeque;
+use std::collections::HashSet;
 
 fn act2_5_1_dfs(v: usize, c: i32, color: &mut Vec<i32>, graph_list: &Vec<Vec<usize>>) -> bool {
 	color[v] = c;
@@ -100,6 +101,112 @@ fn act2_5_2(n: usize, r: usize, g_list: &Vec<Vec<Edge>>) -> usize {
 	dist2[n - 2].unwrap()
 }
 
+#[derive(Clone)]
+struct UVEdge{
+	u: usize,
+	v: usize,
+	cost: i32
+}
+
+impl UVEdge{
+	fn new(u: usize, v: usize, cost: i32) -> UVEdge {
+		UVEdge{
+			u: u,
+			v: v,
+			cost: cost
+		}
+	}
+	fn comp(&mut self, e: UVEdge) -> bool {
+		self.cost < e.cost
+	}
+}
+
+struct UnionFind {
+    par: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> UnionFind {
+        let mut vec = vec![0;n];
+        for i in 0..n {
+            vec[i] = i;
+        }
+        UnionFind {
+            par : vec,
+            rank : vec![0;n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if x == self.par[x] {
+            x
+        }else{
+            let res = self.find(self.par[x]);
+            self.par[x] = res;
+            res
+        }
+    }
+
+    fn same(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
+
+    fn unite(&mut self, a: usize, b: usize){
+        let apar = self.find(a);
+        let bpar = self.find(b);
+		if apar == bpar{
+			return
+		}
+        if self.rank[apar] < self.rank[bpar] {
+            self.par[apar] = bpar;
+        }else{
+            self.par[bpar] = apar;
+            if self.rank[apar] == self.rank[bpar] {
+                self.rank[apar] += 1;
+            }
+        }
+    }
+	fn debug(&mut self){
+		println!("par:{:?}, rank:{:?}", self.par, self.rank);
+	}
+}
+
+fn kruskal(es: &mut Vec<UVEdge>, v: usize, e:usize) -> i32 {
+	es.sort_by(|a, b| a.cost.partial_cmp(&b.cost).unwrap());
+	let mut uf = UnionFind::new(v);
+	let mut res = 0;
+	for i in 0..e {
+		let edge = es[i].clone();
+		if !uf.same(edge.u, edge.v){
+			uf.unite(edge.u, edge.v);
+			res += edge.cost;
+		}
+	}
+	return res
+}
+
+fn act2_5_3(n: usize, m: usize, r: usize, xyd: &Vec<Vec<usize>>) -> i32 {
+	let mut x_list = Vec::new();
+	let mut y_list = Vec::new();
+	let mut d_list = Vec::new();
+	let mut vs = HashSet::new();
+
+	for x in xyd {
+		x_list.push(x[0]);
+		y_list.push(x[1]);
+		d_list.push(x[2]);
+		vs.insert(x[0]);
+		vs.insert(x[1]);
+	}
+	let mut es = Vec::new();
+	for i in 0..r {
+		es.push(UVEdge::new(x_list[i], n + y_list[i], -(d_list[i] as i32)))
+	}
+	let result = (10000i32 * ((n + m) as i32) + kruskal(&mut es, vs.len(), x_list.len()));
+	return result
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -122,5 +229,15 @@ mod tests {
 												  Edge::new(4,100)],
 											 vec![Edge::new(2,200),
 												  Edge::new(4,100)]]));
+	}
+	fn act2_5_3_test(){
+		assert_eq!(71071, act2_5_3(5,5,8,&vec![vec![4,3,6831],
+											   vec![1,3,4583],
+											   vec![0,0,6592],
+											   vec![0,1,3063],
+											   vec![3,3,4975],
+											   vec![1,3,2049],
+											   vec![4,2,2104],
+											   vec![2,2,781]]));
 	}
 }
